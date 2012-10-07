cece1b84a97f012a3ee185528d0dd3d41005a35f
andre.knoerig@gmail.com <andre.knoerig@gmail.com@85f94c43-f63a-0410-9d22-21439860c375>
2012-10-07 15:37:10 +0000

    more project/part parsing and interlinking
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@6527 85f94c43-f63a-0410-9d22-21439860c375

--- ploneweb/src/fritzing.parts/fritzing/parts/part.py
+++ ploneweb/src/fritzing.parts/fritzing/parts/part.py
@@ -1,10 +1,11 @@
+from datetime import date
 from five import grok
 from zope import schema
-from plone.directives import form
+from plone.directives import form, dexterity
 
 from zope.interface import Invalid
 
-from zope.schema import Text, ASCII, ASCIILine
+from zope.schema import Text, ASCII, ASCIILine, Date, Dict, Id, List
 from plone.app.textfield import RichText
 from plone.namedfile.field import NamedBlobFile
 from z3c.relationfield.schema import RelationList, RelationChoice
@@ -34,19 +35,14 @@ def fritzingFilenameIsValid(fileField):
 class IPart(form.Schema):
     """A Fritzing Part
     """
-    title = schema.ASCIILine(
-        title=_(u"Name"),
-        description=_(u"The name of the part"),
-    )
-    description = Text(
-        title=_(u"Teaser"),
-        description=_(u"Give a one-line description of this part"),
-    )
-    body = RichText(
-        title=_(u"Description"),
-        description=_(u"Describe this part as detailed and structured as possible"),
-        required=False,
-    )
+#    title = schema.ASCIILine(
+#        title=_(u"Title"),
+#        description=_(u"The title/name of this part"),
+#    )
+#    description = Text(
+#        title=_(u"Teaser"),
+#        description=_(u"Give a short, teasing description of this part"),
+#    )
     fritzingFile = NamedBlobFile(
         title=_(u"Fritzing Part File"),
         description=_(u"The .fzpz file of the part"),
@@ -59,7 +55,7 @@ class IPart(form.Schema):
     )
     form.omitted('breadboardView')
     breadboardView = ASCII(
-        title=_(u"Breaboard View"),
+        title=_(u"Breadboard View"),
         description=_(u"The SVG graphics for the breadboard view"),
     )
     form.omitted('schematicView')
@@ -78,12 +74,50 @@ class IPart(form.Schema):
         description=_(u"The Fritzing version that this part was created with."),
     )
     form.omitted('moduleId')
-    moduleId = ASCIILine(
+    moduleId = Id(
         title=_(u"Module ID"),
         description=_(u"The unique identifier of this part."),
     )
+    form.omitted('partVersion')
+    partVersion = ASCIILine(
+        title=_(u"Part Version"),
+        description=_(u"The version of this part."),
+    )
+    form.omitted('creationDate')
+    creationDate = Date(
+        title=_(u"Date"),
+        description=_(u"The date when this part was created."),
+    )
+    form.omitted('author')
+    author = ASCIILine(
+        title=_(u"Author"),
+        description=_(u"The author of this part."),
+    )
+    form.omitted('label')
+    label = ASCIILine(
+        title=_(u"Label"),
+        description=_(u"The shorthand label of this part."),
+    )
+    form.omitted('tags')
+    tags = List(
+        title=_(u"Tags"),
+        description=_(u"The tags that describe this part."),
+    )
+    form.omitted('properties')
+    properties = Dict(
+        title=_(u"Properties"),
+        description=_(u"The properties of this part."),
+    )
+    form.omitted('connectors')
+    connectors = Dict(
+        title=_(u"Connectors"),
+        description=_(u"The connectors of this part."),
+    )
+    # TODO
+    #connectors
+    #buses
 
-class View(grok.View):
+class View(dexterity.DisplayForm):
     """Default view (called "@@view"") for a part.
     
     The associated template is found in part_templates/view.pt.
@@ -170,5 +204,34 @@ def extractFZPZ(part, event):
             _(u"Missing required 'module' root element in fzp.")
         )
 
+    # TODO make error tolerant
     part.fritzingVersion = root.getAttribute('fritzingVersion')
     part.moduleId = root.getAttribute('moduleId')
+    part.partVersion = getText(root.getElementsByTagName('version')[0].childNodes)
+    part.author = getText(root.getElementsByTagName('author')[0].childNodes)
+    part.title = getText(root.getElementsByTagName('title')[0].childNodes)
+    part.label = getText(root.getElementsByTagName('label')[0].childNodes)
+    # TODO part.date = date(getText(root.getElementsByTagName('date')[0].childNodes))
+    part.description = getText(root.getElementsByTagName('description')[0].childNodes)
+    part.tags = []
+    for tagXML in root.getElementsByTagName('tag'):
+        part.tags.append(getText(tagXML.childNodes))
+    part.properties = dict()
+    for propertyXML in root.getElementsByTagName('property'):
+        part.properties[propertyXML.getAttribute('name')] = getText(propertyXML.childNodes)
+    part.connectors = dict()
+    for connectorXML in root.getElementsByTagName('connector'):
+        descriptionXML = connectorXML.getElementsByTagName('description')[0]
+        part.connectors[connectorXML.getAttribute('name')] = getText(descriptionXML.childNodes)
+            
+
+    # TODO
+    # more connector knowledge?
+    # buses
+
+def getText(nodelist):
+    rc = []
+    for node in nodelist:
+        if node.nodeType == node.TEXT_NODE:
+            rc.append(node.data)
+    return ''.join(rc) 
--- ploneweb/src/fritzing.projects/fritzing/projects/project.py
+++ ploneweb/src/fritzing.projects/fritzing/projects/project.py
@@ -1,14 +1,19 @@
 from five import grok
 from zope import schema
-from plone.directives import form
-
+from plone.directives import form, dexterity
 from zope.interface import Invalid
-
-from zope.schema import Int, Text
+from zope.schema import ASCII, ASCIILine, Int, SourceText
 from plone.app.textfield import RichText
 from plone.namedfile.field import NamedBlobFile, NamedBlobImage
 from z3c.relationfield.schema import RelationList, RelationChoice
 from plone.formwidget.contenttree import ObjPathSourceBinder
+from zope.lifecycleevent.interfaces import IObjectCreatedEvent, IObjectModifiedEvent
+from Products.CMFCore.utils import getToolByName
+from zope.app.component.hooks import getSite
+
+from Acquisition import aq_inner
+import zipfile, xml.dom.minidom, xml.dom
+from cStringIO import StringIO
 
 from fritzing.projects import FritzingProjectsMessageFactory as _
 from fritzing.parts.part import IPart
@@ -49,6 +54,27 @@ class IProject(form.Schema):
         description=_(u"The Fritzing .fzz file of your project"),
         constraint=isFritzingSketch,
     )
+    form.omitted('breadboardView')
+    breadboardView = ASCII(
+        title=_(u"Breadboard View"),
+        description=_(u"The SVG graphics for the breadboard view"),
+    )
+    form.omitted('schematicView')
+    schematicView = ASCII(
+        title=_(u"Schematic View"),
+        description=_(u"The SVG graphics for the schematic view"),
+    )
+    form.omitted('pcbView')
+    pcbView = ASCII(
+        title=_(u"PCB View"),
+        description=_(u"The SVG graphics for the pcb view"),
+    )
+    form.omitted('code')
+    code = SourceText(
+        title=_(u"Code"),
+        description=_(u"The embedded microcontroller code"),
+    )
+    form.omitted('parts')
     parts = RelationList(
         title=_(u"Parts"),
         description=_(u"Parts used in this project"),
@@ -59,6 +85,28 @@ class IProject(form.Schema):
             ),
         required=False,
     )
+    form.omitted('customParts')
+    customParts = RelationList(
+        title=_(u"Custom Parts"),
+        description=_(u"Custom parts used in this project"),
+        value_type=RelationChoice(
+                source=ObjPathSourceBinder(
+                        object_provides=IPart.__identifier__
+                    ),
+            ),
+        required=False,
+    )
+    form.omitted('fritzingVersion')
+    fritzingVersion = ASCIILine(
+        title=_(u"Fritzing Version"),
+        description=_(u"The Fritzing version that this part was created with."),
+    )
+    form.omitted('numberOfDownloads')
+    numberOfDownloads = Int(
+        title = _(u"Downloads"),
+        description = _(u"How often this project has been downloaded"),
+        default = 0
+    )
     form.omitted('numberOfViews')
     numberOfViews = Int(
         title = _(u"Views"),
@@ -66,7 +114,7 @@ class IProject(form.Schema):
         default = 0
     )
 
-class View(grok.View):
+class View(dexterity.DisplayForm):
     """Default view (called "@@view"") for a project.
     
     The associated template is found in project_templates/view.pt.
@@ -80,4 +128,106 @@ class View(grok.View):
         """Called before rendering the template for this view
         """        
         self.context.numberOfViews += 1
+
+
+@grok.subscribe(IProject, IObjectCreatedEvent)
+@grok.subscribe(IProject, IObjectModifiedEvent)
+def extractFZZ(project, event):
+    fileData = StringIO(project.fritzingFile.data)
+    zf = None
+    try:
+        zf = zipfile.ZipFile(fileData) 
+    except:
+        raise Invalid(
+            _(u"This fzz file seems corrupted.")
+        )
+        
+    customPartFileNames = []
+    codeFileNames = []
+    fzXML = None
+    for i, name in enumerate(zf.namelist()):   
+        if name.endswith('.fz'):
+            fzXML = zf.read(name)
+        elif name.endswith('.fzp'):
+            customPartFileNames.append(name)
+        elif name.endswith('.fzz'):
+            continue
+            # ignore: artefact from renaming
+        elif name.endswith('.svg'):
+            continue
+            # TODO: custom part or board graphics
+        else:
+            codeFileNames.append(name) 
+            # TODO: this should be checked against those listed in the .fz
+    if not fzXML:
+        raise Invalid(
+            _(u"No Fritzing sketch (.fz) found in fzz.")
+        )
     
+    # TODO Generate SVGs
+    #~ breadboardSVG = None
+    #~ schematicSVG = None
+    #~ pcbSVG = None
+    # Run Fritzing to load this fzz and export all SVGs
+    project.breadboardView = ""
+    project.schematicView = ""
+    project.pcbView = ""
+    
+    # Parse FZ
+    dom = None
+    try:
+        dom = xml.dom.minidom.parseString(fzXML)
+    except:
+        raise Invalid(
+            _(u"The fz is not a valid XML file.")
+        )
+    if dom == None:
+        raise Invalid(
+            _(u"The fz is not a valid XML file (2).")
+        )
+    root = dom.documentElement
+    if root.tagName != 'module':
+        raise Invalid(
+            _(u"Missing required 'module' root element in fzp.")
+        )
+
+    project.fritzingVersion = root.getAttribute('fritzingVersion')
+    
+    # Parse parts
+    project.parts = []
+    instances = root.getElementsByTagName('instance')
+    partModuleIds = dict()
+    ignoreIds = ('WireModuleId', 
+        'LogoImageModuleID', 
+        'TwoLayerRectanglePCBModuleID',
+        '423120090505', 
+        '423120090505_2')
+    catalog = getToolByName(getSite(), 'portal_catalog')
+    for instance in instances:
+        partModuleId = instance.getAttribute('moduleIdRef')
+        if (partModuleId not in ignoreIds):
+            if (not partModuleIds.has_key(partModuleId )):
+                partModuleIds[partModuleId ] = 1
+                results = catalog(
+                    object_provides=IPart.__identifier__,
+                    path='/',
+                    moduleId=partModuleId)
+                for brain in results:
+                    obj = brain.getObject()
+                    project.parts.append(obj)
+            else:
+                partModuleIds[partModuleId ] += 1
+    
+    # TODO percentage of routed connections in each view
+    
+    # TODO handle custom parts (as children of this project)
+    
+    # Parse code files
+    project.code = ""
+    for codeFileName in codeFileNames:
+        code = zf.read(codeFileName)
+        project.code += codeFileName
+        project.code += '\n'
+        project.code += code
+        project.code += '\n'
+    #TODO rather add as children of this project?
